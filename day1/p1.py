d6f25a37c6784359d1c4f465b7a874c3976db738
John Carter <mrjohndcarter@gmail.com>
2020-12-06 17:09:51 -0500

    Refactor into function for puzzle 2

--- day1/p1.py
+++ day1/p1.py
@@ -1,3 +1,11 @@
+def find_x_y_for_sum(input_data, search_sum):
+    while input_data:
+        current_x = input_data.pop()
+        search_y = search_sum - current_x
+        if search_y in input_data:
+            return current_x, search_y
+    return None
+
 
 def main():
 
@@ -6,17 +14,12 @@ def main():
     with open('input.p1', 'r') as f:
         input_data = [int(x) for x in f]
 
-    # find x + y == 2020
+    result_tuple = find_x_y_for_sum(input_data, 2020)
 
-    while input_data:
-        current_x = input_data.pop()
-        search_y = 2020 - current_x
-        if search_y in input_data:
-            print(f'{current_x} + {search_y} = 2020, {current_x} * {search_y} = {current_x * search_y}')
-            exit(0)
-
-    print('Not found')
-    exit(0)
+    if result_tuple:
+        print(f'{result_tuple[0]} + {result_tuple[1]} == 2020; {result_tuple[0]} * {result_tuple[1]} = {result_tuple[0] * result_tuple[1]}')
+    else:
+        print('not found')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
